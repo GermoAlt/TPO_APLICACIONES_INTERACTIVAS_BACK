3d0d0593310d2a4341598cbb9e786185502aced9
German <germanaltairac@hotmail.com>
2022-04-22 03:01:53 -0300

    setup

---
+++ src/config/env.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "port": 3600,
+    "appEndpoint": "http://localhost:3600",
+    "apiEndpoint": "http://localhost:3600",
+    "environment": "dev",
+    "permissionLevels": {
+        "NORMAL_USER": 1,
+        "PAID_USER": 4,
+        "ADMIN": 2048
+    }
+};
